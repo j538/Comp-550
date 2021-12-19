df2e6e7b199f46ea16fd2a27fae5e401efff56bc
laurence2000 <laurence.delisle2000@gmail.com>
2021-12-19 16:26:59 -0500

    Removed accuracy function

--- eval.py
+++ eval.py
@@ -24,25 +24,4 @@ def evaluate_accuracy(original, w_errors, corrected):
                 # failed_corrections: A word with errors was left uncorrected or was wrongly corrected
                 elif o_words[j] != c_words[j] and o_words[j] != e_words[j]:
                     failed_corrections += 1
-
     return correctly_modified, new_errors, failed_corrections
-
-#number of mistakes corrected / total number of mistakes
-#This only checks spelling and does not account for the sense of the corrected words
-def accuracy(corrected, with_errors):
-    num = 0
-    denom = 0
-    spell = SpellChecker()
-    for i in range(len(corrected)):
-        #Tokenize sentence into list of words
-        s_w = word_tokenize(corrected[i])
-        t_w = word_tokenize(with_errors[i])
-        #print(s_w,t_w)
-        #Compute number of mistakes in sentence before and after correction
-        num_mistakes = len(spell.unknown(t_w))
-        num_mistakes_after = len(spell.unknown(s_w))
-        #Update numerator and denominator
-        num += num_mistakes - num_mistakes_after #if this is negative : introduced new errors
-        #print(num_mistakes)
-        denom += num_mistakes
-    return num/denom
