6d21d4a4a4015ebbf4844125634ac5068d050f2a
laurence2000 <laurence.delisle2000@gmail.com>
2021-12-20 09:54:35 -0500

    Cleaned the comments of the code

--- viterbi.py
+++ viterbi.py
@@ -1,15 +1,15 @@
 import numpy as np
-from nltk import word_tokenize
 from numpy.core.fromnumeric import argmax
 from gen_errors import generateAllErrors
-from get_probs import emission_probs, get_single_emission_prob
+from get_probs import get_single_emission_prob
 
+#Running the Viterbi algorithm
 #transitions : dictionary of transition probabilities
 #emissions : dictionary of emission probabilities
 #init : dictionary of initial probabilities
 #N : Total number of hidden states (lenght of dict)
 #T : length of the sentence
-#w : input words of a sentence
+#w : array of words of a sentence
 #dict : all hidden states
 def viterbi(transitions,emissions,init,N,T,w,dict):
     #Create trellis and pointers
@@ -27,13 +27,10 @@ def viterbi(transitions,emissions,init,N,T,w,dict):
         if emissions.get(dict[i]) == None :
             tmp_w = generateAllErrors(dict[i])
             tmp_w.append(dict[i])
-            #tmp_w.append("UNK")
             new_emissions = get_single_emission_prob(dict[i], tmp_w, 0.1)
             emissions[dict[i]] = new_emissions
-            #e_i0 = emissions.get(dict[i]).get(w[0])
             
         if emissions.get(dict[i]).get(w[0]) == None :
-            #e_i0 = emissions.get(dict[i]).get("UNK")
             words_in_emission = emissions.get(dict[i]).keys()
             new_words =[]
             for word in w :
@@ -42,8 +39,7 @@ def viterbi(transitions,emissions,init,N,T,w,dict):
             new_emissions = get_single_emission_prob(dict[i], new_words, 0.1)
             for key in new_emissions:
                 emissions[dict[i]][key] = new_emissions[key]
-            #e_i0 = emissions.get(dict[i]).get(w[0])
-        #else :
+        #Compute trellis value for first column
         e_i0 = emissions.get(dict[i]).get(w[0])
         delta[i,0] = initial*e_i0
 
@@ -68,12 +64,10 @@ def viterbi(transitions,emissions,init,N,T,w,dict):
                 if emissions.get(dict[j]) == None :
                     tmp_w = generateAllErrors(dict[i])
                     tmp_w.append(dict[i])
-                    #tmp_w.append("UNK")
                     new_emissions = get_single_emission_prob(dict[j], tmp_w, 0.1)
                     emissions[dict[j]] = new_emissions
-                    #e_jt = emissions.get(dict[j]).get(w[t])
+
                 if emissions.get(dict[j]).get(w[t]) == None :
-                    #e_jt = emissions.get(dict[j]).get("UNK")
                     words_in_emission = emissions.get(dict[j]).keys()
                     new_words =[]
                     for word in w :
@@ -82,14 +76,12 @@ def viterbi(transitions,emissions,init,N,T,w,dict):
                     new_emissions = get_single_emission_prob(dict[j], new_words, 0.1)
                     for key in new_emissions:
                         emissions[dict[j]][key] = new_emissions[key]
-                    #e_jt = emissions.get(dict[j]).get(w[t])
-                #else:
+                #Store all values for one cell
                 e_jt = emissions.get(dict[j]).get(w[t])
                 c.append(delta[i,t-1]*t_ij*e_jt)
-            #Store best score and the pointer to previous cell
+            #Store best score of this cell and the pointer to previous cell
             delta[j,t] = max(c)
             pointers[j,t] = argmax(c)
-    #print(delta)
     #Return pointers matrix and max of last column
     return (pointers, argmax(delta[:,T-1]))
 
@@ -110,22 +102,3 @@ def recover_path(len,pointers,dict,start):
     for s in path:
         new_s = new_s + " " + s
     return new_s
-
-"""
-Testing using some small data
-pi = {"i" : 0.6, "like" : 0.1, "movies" : 0.3}
-a = {
-    "i" : {"i" : 0.1, "like" : 0.7, "movies" : 0.2},
-    "like" : {"i" : 0.1, "like" : 0.1, "movies" : 0.8},
-    "movies" : {"i" : 0.1, "like" : 0.1, "movies" : 0.8},
-}
-b = {
-    "i" : {"i" : 0.7, "like" : 0.2, "movy" : 0.1},
-    "like" : {"i" : 0.2, "like" : 0.6, "movy" : 0.2},
-    "movies" : {"i" : 0.1, "like" : 0.1, "movy" : 0.8},
-}
-dict = ["i","like","movies"]
-s = "i like movy"
-(p,start) = viterbi(a,b,pi,len(dict),3,word_tokenize(s),dict)
-print(recover_path(3, p, dict, start))
-"""
